4cf766b5f5031434150a95abeeff290cfddbb29f
Trey Hardin <treyhardindesign@gmail.com>
2024-01-09 14:33:58 -0500

    Clients grid scroll

--- src/components/clients/clients.jsx
+++ src/components/clients/clients.jsx
@@ -1,4 +1,4 @@
-import { For, createEffect, createResource } from 'solid-js'
+import { For, Show, createEffect, createResource, onMount } from 'solid-js'
 import styles from './clients.module.css'
 import { getClients, urlFor } from '../../utilities/sanity-client'
 import { observer } from '../../utilities/intersectionObserver'
@@ -9,45 +9,81 @@ import { lenis } from '../../App';
 export default function Clients() {
 
     let cards;
+    let scrollerRow = [];
 
-    let boundingBox
+    const initScrollAnimation = (el) => {
 
+        // Check for reduced motion preference
+        if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
 
-    lenis.on('scroll', (e) => {
-    // console.log(e)
-        // console.log(cards)
-        // boundingBox = cards.getBoundingClientRect()
-        // const scrollOffset = boundingBox.top / window.innerHeight
-        // const scrollOffset = window.scrollY / window.innerHeight
-        // const scrollOffset = 1 - boundingBox.bottom / window.innerHeight
-        // const scrollOffset = 1 - Math.max(0, Math.min(boundingBox.bottom, window.innerHeight)) / window.innerHeight
-        // console.log(1 / scrollOffset)
-        // console.log(scrollOffset)
-        // console.log(cards.clientWidth)
-        // console.log(window.getComputedStyle(cards, null).getPropertyValue("width"))
-        // console.log(boundingBox.top / window.innerHeight)
-        // cards.style.translate = `${scrollOffset * (boundingBox.width - window.innerWidth)}px 0`;
-    })
+            scrollerRow.forEach((row, i) => {
+
+                const scrollerChildren = Array.from(row.children)
+
+                // console.log(scrollerChildren)
+    
+                scrollerChildren.forEach((child, i) => {
+                    // console.log(child)
+                    const clonedElement = child.cloneNode(true)
+                    clonedElement.setAttribute("aria-hidden", true)
+                    row.appendChild(clonedElement)
+                })
+            })
+
+            
+        }
+    }
+    
 
     const [ data ] = createResource(getClients)
 
-    // createEffect(() => {
-    //     // console.log(data())
-    //     // observer.observe(clients)
-    //     console.log(cards)
+    createEffect(() => {
 
-    // })
+        if (!data.loading && !data.error && scrollerRow.length > 0) {
+            initScrollAnimation()
+            // console.log(data())
+        }
+
+    })
 
     const minScale = 0.5
     
     return (
         <section class={styles.clients} ref={cards}>
-            <For each={data()}>{(client, i) =>
-                <div class={styles.clientCard} key={i()} style={`--index: ${i() + 1}; --random: ${Math.random() * minScale + minScale}`}>
-                    <img width="500" height="300" class={styles.clientImage} src={urlFor(client.image).width(1200)} />
-                    <img width="500" height="500" class={styles.clientLogo} src={urlFor(client.logo).width(1000)} />
-                </div>
-            }</For>
+
+            <div class={styles.clientCardRow} ref={scrollerRow[0]}>
+                <For each={data()}>{(client, i) =>
+                    <Show when={i() <= data().length / 3}>
+                    <div class={styles.clientCard} key={i()} style={`--index: ${i() + 1}; --random: ${Math.random() * minScale + minScale}`}>
+                        <img width="500" height="300" class={styles.clientImage} src={urlFor(client.image).width(1200)} />
+                        <img width="500" height="500" class={styles.clientLogo} src={urlFor(client.logo).width(1000)} />
+                    </div>
+                    </Show>
+                }</For>
+            </div>
+
+            <div class={styles.clientCardRow} ref={scrollerRow[1]}>
+                <For each={data()}>{(client, i) =>
+                    <Show when={i() > data().length / 3 && i() <= data().length / 3 * 2}>
+                        <div class={styles.clientCard} key={i()} style={`--index: ${i() + 1}; --random: ${Math.random() * minScale + minScale}`}>
+                            <img width="500" height="300" class={styles.clientImage} src={urlFor(client.image).width(1200)} />
+                            <img width="500" height="500" class={styles.clientLogo} src={urlFor(client.logo).width(1000)} />
+                        </div>
+                    </Show>
+                }</For>
+            </div>
+
+            <div class={styles.clientCardRow} ref={scrollerRow[2]}>
+                <For each={data()}>{(client, i) =>
+                    <Show when={i() > data().length / 3 * 2}>
+                        <div class={styles.clientCard} key={i()} style={`--index: ${i() + 1}; --random: ${Math.random() * minScale + minScale}`}>
+                            <img width="500" height="300" class={styles.clientImage} src={urlFor(client.image).width(1200)} />
+                            <img width="500" height="500" class={styles.clientLogo} src={urlFor(client.logo).width(1000)} />
+                        </div>
+                    </Show>
+                }</For>
+            </div>
+
         </section>
     )
 }
